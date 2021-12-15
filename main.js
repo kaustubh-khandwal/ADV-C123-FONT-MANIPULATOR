941618246bfeb58a49494e0671e4c68a4aec50b8
kaustubh-khandwal <76221706+kaustubh-khandwal@users.noreply.github.com>
2021-12-15 19:42:30 +0530

    part 2

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 function setup() {
     canvas = createCanvas(550,550);
     canvas.position(600,150);
+    video = createCapture(500,500)
+    posenet = ml5.poseNet(video,modelloaded);
+    posenet.on('pose',gotposes);
+}
+
+function modelloaded() {
+    console.log("poseNet is intialized")
+}
+
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results)
+    }
 }
